18fe0f79f2b85d6e75e4fb278de0bb2f8e0b21c6
oversanzero <oversanzero@gmail.com>
2024-05-28 05:42:19 +0000

    ⬅️ Changing BookCard

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,14 +1,25 @@
 export default function BookCard({ book }) {
   return (
-    <div className="flex flex-col items-center justify-center p-4 rounded-lg shadow-lg bg-white">
+    <div className="flex flex-row w-[80vw] h-52 my-3 rounded-lg shadow-lg gap-3 cursor-pointer">
       <img
         src={book.cover}
         alt={book.title}
-        className="w-32 h-48 object-cover rounded-lg"
+        className="w-32 h-52 object-cover rounded-lg"
       />
-      <h2 className="text-2xl font-bold mt-4">{book.title}</h2>
-      <p className="text-gray-500 mt-2">Release Date: {book.releaseDate}</p>
-      <p className="text-gray-500 mt-2">Pages: {book.pages}</p>
+      <div className="flex flex-col">
+        <h2 className="text-2xl font-bold mt-4 text-wrap">{book.title}</h2>
+        <p className="text-sm text-gray-600 mt-2 text-wrap h-20 overflow-hidden whitespace-nowrap overflow-ellipsis">
+          {book.description}
+        </p>
+        <div className="flex flex-row self-end items-end gap-3 font-medium bg-red-500">
+          <p className="text-gray-900 bg-gray-300 px-2 py-1 rounded-lg">
+            {book.releaseDate}
+          </p>
+          <p className="text-gray-900 bg-gray-300 px-2 py-1 rounded-lg">
+            {book.pages}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
